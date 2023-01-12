423900f35c1190b5ebc07835904b241ee324638a
IschukM <dzubak@ukr.net>
2023-01-13 00:29:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,14 +14,13 @@ inputEl.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
 
 function onInputChange(e) {
   outputClear();
-  if (e.target.value.trim() != " ") {
+  if (e.target.value.trim() != ' ') {
     fetchCountries(e.target.value.trim())
-    .then (renderHTML)
+      .then(renderHTML)
       .catch(error => {
         Notify.failure('Oops, there is no country with that name');
-console.log(error);
-      
-    })
+        console.log(error);
+      });
   }
 }
 
@@ -30,17 +29,28 @@ function onInputChange() {
   divEl.innerHTML = ' ';
 }
 
-function renderHTML(coutry) {
-  if (coutry > 10) {
+function renderHTML(country) {
+  if (country > 10) {
     Notiflix.Notify.info(
       'Too many matches found. Please enter a more specific name.'
     );
-  }
-  else if (coutry >= 2 && coutry <= 10) {
-  
+  } else if (country >= 2 && country <= 10) {
+    renderCountriesList(country);
   }
 }
 // fields=name,capital,population,flags,languages`
 // більше ніж 10 країн
 // від 2 - х до 10 - и країн -прапора та назви країни
 // це масив з однією країною -прапор, назва, столиця, населення і мови
+
+function renderCountriesList(countriesArr) {
+  const markupList = countriesArr
+    .map(({ flags, name }) => {
+      return `<li>
+        <img src="${flags.svg}" width="50px">
+        <p>${name.official}</p>
+        </li>`;
+    })
+    .join('');
+  listEl.insertAdjacentHTML('beforeend', markupList);
+}
