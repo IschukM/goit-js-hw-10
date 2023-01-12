b40019e62dc49eea8a5e916316a069f89f977573
IschukM <dzubak@ukr.net>
2023-01-12 23:24:14 +0200

    render

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import './css/styles.css';
+import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box');
+const listEl = document.querySelector('.country-list');
+const divEl = document.querySelector('country-info');
+
+inputEl.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
+
+function onInputChange(e) {
+  outputClear();
+  if (e.target.value.trim() != " ") {
+    fetchCountries(e.target.value.trim());
+    .then (renderHTML)
+      .catch(error => {
+        Notify.failure('Oops, there is no country with that name');
+console.log(error);
+      
+    })
+  }
+}
+
+function onInputChange() {
+  listEl.innerHTML = ' ';
+  divEl.innerHTML = ' ';
+}
+
+function renderHTML(coutreis)
